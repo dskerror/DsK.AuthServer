00242a1afa43f8e0aabd9e80c87171eee2e496b5
DsKErrOr <dskerror@gmail.com>
2023-01-23 07:19:40 -0400

    users ef

--- BlazorWASMCustomAuth.Security.Infrastructure/SecurityServiceEF/Users.cs
+++ BlazorWASMCustomAuth.Security.Infrastructure/SecurityServiceEF/Users.cs
@@ -1,21 +1,6 @@
-﻿using BlazorWASMCustomAuth.Database;
-using BlazorWASMCustomAuth.PagingSortingFiltering;
-using BlazorWASMCustomAuth.Security.EntityFramework.Models;
+﻿using BlazorWASMCustomAuth.Security.EntityFramework.Models;
 using BlazorWASMCustomAuth.Security.Shared;
 using BlazorWASMCustomAuth.Validations;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.DirectoryServices.AccountManagement;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http.ModelBinding;
 
 namespace BlazorWASMCustomAuth.SecurityEF.Infrastructure
 {
@@ -25,11 +10,6 @@ namespace BlazorWASMCustomAuth.SecurityEF.Infrastructure
         {
             APIResult result = new APIResult(model);
 
-            if (!ModelState.IsValid)
-            {
-                return UnprocessableEntity(ModelState);
-            }
-
             var newUser = new User()
             {
                 Username = model.Username,
@@ -37,212 +17,48 @@ namespace BlazorWASMCustomAuth.SecurityEF.Infrastructure
                 Name = model.FullName
             };
 
-            var x = db.Users.ToList();
-            result.Result = x;
-            //result.ModelValidationResult = model.ValidateModel();
-
-            //if (!result.ModelValidationResult.IsValid)
-            //{
-            //    result.HasError = true;
-            //    result.Message = "See Model Validations";
-            //    return result;
-            //}
-
-            //var UserExists = UserVerifyExistsByUsername(model.Username);
-            //if (UserExists)
-            //{
-            //    result.Message = "Username already exists.";
-            //    result.HasError = true;
-            //    return result;
-            //}
-
-            //var EmailExists = UserVerifyExistsByEmail(model.Email);
-            //if (EmailExists)
-            //{
-            //    result.Message = "Email already exists.";
-            //    result.HasError = true;
-            //    return result;
-            //}
-
-            //var dbresult = dm.ExecScalarSP("sp_UserCreate",
-            //    "Username", model.Username ?? "",
-            //    "Email", model.Email ?? "",
-            //    "Name", model.FullName ?? "");
-            //result.Result = dbresult.Result;
-            //result.HasError = dbresult.HasError;
-            //result.Exception = dbresult.Exception;
-            //result.Message = "User Created";
+            db.Users.Add(newUser);
+            db.SaveChanges();
+            result.Result = newUser;
 
             return result;
         }
-        //public UsersGetDTO UsersGet(PagingSortingFilteringRequest request)
-        //{
-        //    var usersCountResult = dm.ExecScalarSP("sp_UsersCountGet");
-        //    var response = new UsersGetDTO();
 
-        //    response.TotalRows = (int)usersCountResult.Result;
-        //    if (request != null)
-        //    {
-        //        response.PageSize = request.PageSize;
-        //        response.CurrentPage = request.CurrentPage;
-        //    }
+        public APIResult UsersGet()
+        {
+            APIResult result = new APIResult(null);
+            result.Result = db.Users.ToList();
+            return result;
+        }
 
-        //    var list = new List<UserDTO>();
-        //    var UserListDt = dm.ExecDataTableSP("sp_UserList", "PageSize", response.PageSize, "OffSet", response.OffSet());
-        //    foreach (DataRow users in UserListDt.Rows)
-        //    {
-        //        list.Add(new UserDTO()
-        //        {
-        //            Id = int.Parse(users["Id"].ToString() ?? ""),
-        //            Name = users["Name"].ToString(),
-        //            Username = users["Username"].ToString(),
-        //            Email = users["Email"].ToString()
-        //        });
-        //    }
-        //    response.UserList = list;
-        //    return response;
-        //}
-        //public APIResult UsersGetNew(PagingSortingFilteringRequest model)
-        //{
-        //    APIResult result = new APIResult(model);
+        public APIResult UserUpdate(UserUpdateDto model)
+        {
+            APIResult result = new APIResult(model);
 
-        //    //var usersCountResult = dm.ExecScalarSP("sp_UsersCountGet");
-        //    //var response = new UsersGetDTO();
+            var user = db.Users.FirstOrDefault(x => x.Id == model.Id);
 
-        //    //response.TotalRows = (int)usersCountResult.Result;
-        //    //if (request != null)
-        //    //{
-        //    //    response.PageSize = request.PageSize;
-        //    //    response.CurrentPage = request.CurrentPage;
-        //    //}
+            if (user != null)
+            {
+                user.Email = model.Email;
+                user.Name = model.Name;
 
-        //    var list = new List<UserModel>();
-        //    var UserListDt = dm.ExecDataTableSP("sp_UserList", model.SQLParameters()); //model.SQLParameters()
-        //    foreach (DataRow users in UserListDt.Rows)
-        //    {
-        //        list.Add(new UserModel()
-        //        {
-        //            Id = int.Parse(users["Id"].ToString() ?? ""),
-        //            Name = users["Name"].ToString(),
-        //            Username = users["Username"].ToString(),
-        //            Email = users["Email"].ToString()
-        //        });
-        //    }
-        //    result.Result = list;
+            }
 
-        //    return result;
-        //}
-        //public APIResult UserUpdate(UserUpdateDto model)
-        //{
-        //    APIResult result = new APIResult(model);
+            db.SaveChanges();
 
-        //    result.ModelValidationResult = model.ValidateModel();
-        //    if (!result.ModelValidationResult.IsValid)
-        //    {
-        //        result.HasError = true;
-        //        result.Message = "See Model Validations";
-        //        return result;
-        //    }
+            result.Result = user;
+            return result;
 
-        //    var userToUpdate = UserGetBy("Id", model.Id.ToString());
-        //    if (userToUpdate == null)
-        //    {
-        //        result.HasError = true;
-        //        result.Message = "Update Failed - Id not found.";
-        //        return result;
-        //    }
-
-        //    var userWithSameEmail = UserGetBy("Email", model.Email);
-        //    if (userWithSameEmail != null)
-        //    {
-        //        if (userToUpdate.Id != userWithSameEmail.Id)
-        //        {
-        //            result.Message = "Email already exists.";
-        //            result.HasError = true;
-        //            return result;
-        //        }
-        //    }
-
-        //    DateTime? LockoutEnd = null;
-
-        //    try
-        //    {
-        //        LockoutEnd = DateTime.Parse(model.LockoutEnd);
-        //    }
-        //    catch (Exception)
-        //    {
-        //        //ignore - ugly
-        //    }
-
-
-        //    var dbresult = dm.ExecNonQuerySP("sp_UserUpdate",
-        //        "Id", model.Id,
-        //        "Email", model.Email,
-        //        "Name", model.Name,
-        //        "EmailConfirmed", model.EmailConfirmed,
-        //        "LockoutEnd", LockoutEnd,
-        //        "LockoutEnabled", model.LockoutEnabled,
-        //        "AccessFailedCount", model.AccessFailedCount,
-        //        "TwoFactorEnabled", model.TwoFactorEnabled
-        //        );
-        //    result.Result = dbresult.Result;
-        //    result.HasError = dbresult.HasError;
-        //    result.Exception = dbresult.Exception;
-        //    result.Message = "User Updated";
-
-        //    return result;
-        //}
-        //public APIResult UserDelete(int id)
-        //{
-        //    APIResult result = new APIResult(id);
-        //    //TODO: cascade delete or disable user?
-        //    //var dbresult = dm.ExecScalarSP("sp_UserDelete", "Id", id);
-        //    //result.Result = dbresult.Result;
-        //    //result.HasError = dbresult.HasError;
-        //    //result.Exception = dbresult.Exception;
-        //    //result.Message = "User Deleted";
-        //    ////TODO: Find if no record was update to give error message in this and other update methods
-
-        //    return result;
-        //}
-        //public bool UserVerifyExistsByUsername(string username)
-        //{
-        //    var dt = dm.ExecDataTableSP("sp_UserList", "Username", username);
-
-        //    if (dt.Rows.Count == 0)
-        //        return false;
-
-        //    return true;
-        //}
-        //public bool UserVerifyExistsByEmail(string email)
-        //{
-        //    var dt = dm.ExecDataTableSP("sp_UserList", "Email", email);
-
-        //    if (dt.Rows.Count == 0)
-        //        return false;
-
-        //    return true;
-        //}
-
-
-        //private UserModel UserGetBy(string column, string value)
-        //{
-        //    UserModel user = new UserModel();
-
-        //    var UserDt = dm.ExecDataTableSP("sp_UserList", column, value);
-        //    foreach (DataRow dr in UserDt.Rows)
-        //    {
-        //        user.Id = int.Parse(dr["Id"].ToString() ?? "");
-        //        user.Name = dr["Name"].ToString();
-        //        user.Username = dr["Username"].ToString();
-        //        user.Email = dr["Email"].ToString();
-        //        user.Permissions = GetUserPermissions(dr["Username"].ToString());
-        //    }
-
-        //    if (user.Id == 0)
-        //        return null;
-        //    return user;
-        //}
+        }
+        public APIResult UserDelete(int id)
+        {
+            APIResult result = new APIResult(id);
+            var user = db.Users.FirstOrDefault(x => x.Id == id);
+            db.Users.Remove(user);
+            var x = db.SaveChanges();
+            result.Result = x;
+            return result;
+        }
     }
 }
 
--- BlazorWASMCustomAuth.Server/Controllers/Security/UsersEFController.cs
+++ BlazorWASMCustomAuth.Server/Controllers/Security/UsersEFController.cs
@@ -18,64 +18,36 @@ namespace BlazorWASMCustomAuth.Server.Controllers.Security
         }
 
         [HttpPost]
-        //[Authorize(Roles = "admin,UserCreate")]
-        //[Route("UserCreate")]
+        //[Authorize(Roles = "admin,UserCreate")]        
         public IActionResult UserCreate(UserCreateDto model)
         {
             var result = SecurityService.UserCreate(model);
             return Ok(result);
         }
 
-        //[HttpGet]
-        ////[Authorize(Roles = "admin,UsersGet")]
-        ////[Route("UsersGet")]
-        //public IActionResult UsersGet([FromQuery] PagingSortingFilteringRequest request)
-        //{
-        //    return Ok(SecurityService.UsersGet(request));
-        //}
+        [HttpGet]
+        //[Authorize(Roles = "admin,UsersGet")]
+        public IActionResult UsersGet()
+        {
+            return Ok(SecurityService.UsersGet());
+        }
 
-        //[HttpGet]
-        ////[Authorize(Roles = "admin,UsersGet")]
-        //[Route("UsersGetNew")]
-        //public IActionResult UsersGetNew([FromQuery] PagingSortingFilteringRequest model)
-        //{
-        //    return Ok(SecurityService.UsersGetNew(model));
-        //}
+        [HttpPut]
+        //[Authorize(Roles = "admin,UserDelete")]
+        public IActionResult UserUpdate(UserUpdateDto model)
+        {
+            var result = SecurityService.UserUpdate(model);
+            return Ok(result);
+        }
 
-        //[HttpPut]
-        ////[Authorize(Roles = "admin,UserDelete")]
-        //public IActionResult UserUpdate(UserUpdateDto model)
-        //{
-        //    var result = SecurityService.UserUpdate(model);
-        //    return Ok(result);
-        //}
+        [HttpDelete]
+        //[Authorize(Roles = "admin,UserDelete")]
+        public IActionResult UserDelete(int id)
+        {
+            var result = SecurityService.UserDelete(id);
+            return Ok(result);
+        }
 
-        //[HttpDelete]
-        ////[Authorize(Roles = "admin,UserDelete")]
-        //public IActionResult UserDelete(int id)
-        //{
-        //    var result = SecurityService.UserDelete(id);
-        //    return Ok(result);
-        //}
-    
-
-        //[HttpGet]
-        ////[Authorize(Roles = "admin,UserUserVerifyExistsByUsernamesGet")]
-        //[Route("VerifyExistsByUsername")]
-        //public IActionResult UserVerifyExistsByUsername([FromQuery] string username)
-        //{
-        //    return Ok(SecurityService.UserVerifyExistsByUsername(username));
-        //}
-
-        //[HttpGet]
-        ////[Authorize(Roles = "admin,UserUserVerifyExistsByUsernamesGet")]
-        //[Route("VerifyExistsByEmail")]
-        //public IActionResult UserVerifyExistsByEmail([FromQuery] string email)
-        //{
-        //    return Ok(SecurityService.UserVerifyExistsByEmail(email));
-        //}
-
-     
     }
 }
 
