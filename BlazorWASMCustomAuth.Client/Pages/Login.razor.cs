85435d8231865df0abb70970bfdc4c919849e759
DsKErrOr <dskerror@gmail.com>
2023-02-01 21:26:05 -0400

    save

---
+++ BlazorWASMCustomAuth.Client/Pages/Login.razor.cs
@@ -0,0 +1,45 @@
+﻿using BlazorWASMCustomAuth.Security.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+
+namespace BlazorWASMCustomAuth.Client.Pages;
+public partial class Login
+{
+    [CascadingParameter] private Task<AuthenticationState> authenticationState { get; set; }
+    private UserLoginDto userLoginModel = new UserLoginDto();
+
+    protected override async Task OnInitializedAsync()
+    {
+
+    }
+
+    private async Task SubmitAsync()
+    {
+        bool result = await securityService.LoginAsync(userLoginModel);
+        if (result)
+        {
+            _navigationManager.NavigateTo("/");
+        }
+    }
+
+    private bool _passwordVisibility;
+    private InputType _passwordInput = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    void TogglePasswordVisibility()
+    {
+        if (_passwordVisibility)
+        {
+            _passwordVisibility = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInput = InputType.Password;
+        }
+        else
+        {
+            _passwordVisibility = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInput = InputType.Text;
+        }
+    }
+}
--- BlazorWASMCustomAuth.Server/Program.cs
+++ BlazorWASMCustomAuth.Server/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<SecurityTablesTestContext>(options =>
@@ -65,8 +65,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	//app.UseSwagger();
+	//app.UseSwaggerUI();
 }
 
 
