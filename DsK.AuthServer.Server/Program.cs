b7640032cde469036713796b3d4183ab2e9bf23e
DsKErrOr <dskerror@gmail.com>
2024-02-15 16:58:10 -0400

    WIP - Fix so that app works locally but also published to an IIS Server.

--- DsK.AuthServer.Server/Program.cs
+++ DsK.AuthServer.Server/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 //builder.Services.AddHttpLogging(logging =>
 //{
@@ -92,11 +92,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//	app.UseSwagger();
-//	app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
 
 //app.UseHttpLogging();
